c4a4971a0310bd5c4076c209575f8693cec9ce37
dyf <dyf@fake.email>
2021-12-04 07:28:02 -0800

    unet

--- model.py
+++ model.py
@@ -2,34 +2,42 @@ import tensorflow as tf
 import tensorflow.keras as tfk
 import tensorflow.keras.layers as tfkl
 
-def downconv_block(x, nf, ks):
-    x = tfkl.Conv1D(filters=nf, kernel_size=ks, padding='same')(x)
-    x = tfkl.Conv1D(filters=nf, kernel_size=ks, strides=2, padding='same')(x)    
+def downconv_block(x, nf, ks):    
+    x = tfkl.Conv1D(filters=nf, kernel_size=ks, padding='same', activation='relu')(x)
+    x = tfkl.Conv1D(filters=nf, kernel_size=ks, strides=2, padding='same', activation='relu')(x)
+    x = tfkl.Dropout(0.1)(x)
     return x
 
-def upconv_block(x, skipx, nf, ks):
-    x = tfkl.Conv1DTranspose(filters=nf, kernel_size=ks, strides=2, padding='same')(x)                
+def upconv_block(x, skipx, nf, ks):    
+    x = tfkl.Conv1DTranspose(filters=nf, kernel_size=ks, strides=2, padding='same', activation='relu')(x)
     x = tfkl.Concatenate()([x, skipx])
-    x = tfkl.Conv1D(filters=nf, kernel_size=ks, padding='same')(x)        
+    x = tfkl.Conv1D(filters=nf, kernel_size=ks, padding='same', activation='relu')(x)
+    #x = tfkl.Dropout(0.1)(x)
     return x
 
-def unet(input_shape, num_layers, minf=20, ks=5):
+def unet(input_shape, nfs, minf=20, ks=7):
+    num_layers = len(nfs)
+
     in_x = tfk.Input(shape=input_shape)
     
     x = in_x
     layer_outputs = [x]
-    for li in range(num_layers):
-        xout = downconv_block(x, minf*2**li, ks)
+    for i in range(num_layers-1):
+        xout = downconv_block(x, nfs[i], ks)
         layer_outputs.append(xout)
         x = xout
+
+    x = tfkl.Conv1D(filters=nfs[-1], kernel_size=ks, padding='same', activation='relu')(x)
+    x = tfkl.Dropout(0.1)(x)    
     
     y1 = x
     y2 = x
-    for li in range(num_layers-1,-1,-1):
-        y1 = upconv_block(y1, layer_outputs[li], minf*2**li, ks)
+        
+    for li in range(num_layers-2,-1,-1):    
+        y1 = upconv_block(y1, layer_outputs[li], nfs[li], ks)
 
-    for li in range(num_layers-1,-1,-1):
-        y2 = upconv_block(y2, layer_outputs[li], minf*2**li, ks)
+    for li in range(num_layers-2,-1,-1):
+        y2 = upconv_block(y2, layer_outputs[li], nfs[li], ks)
 
     y1 = tfkl.Conv1D(filters=2, kernel_size=ks, padding='same', name='vocals')(y1)
     y2 = tfkl.Conv1D(filters=2, kernel_size=ks, padding='same', name='accompaniment')(y2)
@@ -37,5 +45,5 @@ def unet(input_shape, num_layers, minf=20, ks=5):
     return tfk.Model(inputs=in_x, outputs=[y1,y2])
 
 if __name__ == "__main__":
-    model = unet((1000000,2), 4)
+    model = unet((1000000,2), nfs=[10,20,30,40,50])
     print(model.summary())
--- train.py
+++ train.py
@@ -1,18 +1,25 @@
 import model as mixmodel
 import dataset as mixdataset
+import tensorflow.keras as tfk
+
 def train():
-    N = 32
-    epochs = 10
-    batch_size = 16
+    N = 2048
+    epochs = 20
+    batch_size = 4
+    unet_filters = [ 30, 60, 90, 120, 150, 180, 210 ]
 
     ds = mixdataset.MusdbData('D:/MUSDB18/Full')
-    train_ds = ds.random_dataset(N, 3.2, subset='train')
-    test_ds = ds.random_dataset(N, 3.2, subset='test')
+    train_ds = ds.random_dataset(N, 1.0, subset='train')
+    test_ds = ds.random_dataset(batch_size*4, 1.0, subset='test')
 
-    model = mixmodel.unet((131072,2), 4)
-    model.compile(loss=['mse','mse'], optimizer='adam')
+    model = mixmodel.unet((32768,2), unet_filters)
+    model.compile(loss=['mse','mse'], optimizer=tfk.optimizers.Adam(lr=0.001))
     print(model.summary())
-    model.fit(train_ds.batch(batch_size), epochs=epochs, validation_data=test_ds.batch(batch_size))
-    model.save('model')
+
+    my_callbacks = [
+        tfk.callbacks.ModelCheckpoint(filepath='model.{epoch:02d}.h5'),        
+    ]
+
+    model.fit(train_ds.batch(batch_size), epochs=epochs, validation_data=test_ds.batch(batch_size),callbacks=my_callbacks)
 
 if __name__ == "__main__": train()
