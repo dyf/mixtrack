7770e173e7310b959ad75bb8842aee88abf6e395
dyf <dyf@fake.email>
2021-12-17 09:22:42 -0800

    messing with spectrograms, residual blocks

--- model.py
+++ model.py
@@ -1,27 +1,32 @@
 import tensorflow as tf
 import tensorflow.keras as tfk
 import tensorflow.keras.layers as tfkl
+import tensorflow_addons as tfa
+import tensorflow_addons.layers as tfal
+
+import dataset as mixdata
 
 def downconv_block(x, nf, ks):    
+    xres = tfkl.Conv1D(filters=nf, kernel_size=ks, strides=4, padding='same', kernel_initializer='he_normal')(x)
+    x = tfkl.ReLU()(xres)
     x = tfkl.Conv1D(filters=nf, kernel_size=ks, padding='same', kernel_initializer='he_normal')(x)
-    x = tfkl.LeakyReLU()(x)
-    #x = tfkl.Conv1D(filters=nf, kernel_size=ks, padding='same', kernel_initializer='he_normal')(x)
-    #x = tfkl.LeakyReLU()(x)
-    #x = tfkl.MaxPooling1D()(x)
-    x = tfkl.Conv1D(filters=nf, kernel_size=ks, strides=2, padding='same', kernel_initializer='he_normal')(x)
-    x = tfkl.LeakyReLU()(x)
-    #x = tfkl.Dropout(0.1)(x)
+    x = tfkl.ReLU()(x)
+    x = tfkl.Conv1D(filters=nf, kernel_size=ks, padding='same', kernel_initializer='he_normal')(x)
+
+    #x = tfkl.Concatenate()([x, xres])
+    #x = tfal.GELU()(x)
+
     return x
 
-def upconv_block(x, skipx, nf, ks):    
-    x = tfkl.Conv1DTranspose(filters=nf, kernel_size=ks, strides=2, padding='same', kernel_initializer='he_normal')(x)
-    x = tfkl.LeakyReLU()(x)
-    #x = tfkl.UpSampling2D((2,1))(x)    
-    x = tfkl.Concatenate()([x, skipx])
+def upconv_block(x, nf, ks):    
+    xres = tfkl.Conv1DTranspose(filters=nf, kernel_size=ks, strides=4, padding='same', kernel_initializer='he_normal')(x)
+    x = tfkl.ReLU()(xres)
     x = tfkl.Conv1D(filters=nf, kernel_size=ks, padding='same', kernel_initializer='he_normal')(x)
-    x = tfkl.LeakyReLU()(x)
-    #x = tfkl.Conv1D(filters=nf, kernel_size=ks, padding='same', kernel_initializer='he_normal')(x)
-    #x = tfkl.LeakyReLU()(x)
+    x = tfkl.ReLU()(x)
+    x = tfkl.Conv1D(filters=nf, kernel_size=ks, padding='same', kernel_initializer='he_normal')(x)
+    
+    #x = tfkl.Concatenate()([x, xres])
+    #x = tfal.GELU()(x)
     x = tfkl.Dropout(0.1)(x)
     return x
 
@@ -32,23 +37,24 @@ def unet(input_shape, nfs, ks=9):
     
     x = in_x
     layer_outputs = [x]
-    for i in range(num_layers-1):
+    for i in range(num_layers):
         xout = downconv_block(x, nfs[i], ks)
         layer_outputs.append(xout)
         x = xout
 
-    x = tfkl.Conv1D(filters=nfs[-1], kernel_size=ks, padding='same', kernel_initializer='he_normal')(x)
-    x = tfkl.LeakyReLU()(x)
-    x = tfkl.Dropout(0.1)(x)    
+    x = upconv_block(x, nfs[-2], ks)
     
-    for li in range(num_layers-2,-1,-1):    
-        x = upconv_block(x, layer_outputs[li], nfs[li], ks)
+    print(layer_outputs)
+    for li in range(num_layers-1,0,-1):
+        
+        x = tfkl.Concatenate()([x, layer_outputs[li]])
+        x = upconv_block(x, nfs[li], ks)
 
     x = tfkl.Conv1D(filters=2, kernel_size=ks, padding='same', kernel_initializer='he_normal')(x)
-    x = tfkl.LeakyReLU()(x)
+    x = tfal.GELU()(x)
     x = tfkl.Conv1D(filters=2, kernel_size=1, name='target')(x)    
 
-    return tfk.Model(inputs=in_x, outputs=x)
+    return tfk.Model(inputs=in_x, outputs=x, name="unet_unmixer")
 
 def downconv2d_block(x, nf, ks):    
     x = tfkl.Conv2D(nf, kernel_size=ks, padding='same')(x)
@@ -67,36 +73,29 @@ def upconv2d_block(x, skipx, nf, ks):
     x = tfkl.Dropout(0.1)(x)
     return x
 
+def discriminator(input_shape, nfs, ks):
 
-def stereo_stft(x, frame_length, frame_step):
-    x = [ 
-        tf.signal.stft(x[:,:,i], frame_length=frame_length, frame_step=frame_step) 
-        for i in range(x.shape[-1])
-    ]
-
-    x = [
-        tf.stack([tf.math.real(s), tf.math.imag(s)], axis=3) for s in x
-    ]
-
-    x = tf.concat(x, axis=3)
-
-    return x
-
-def stereo_inverse_stft(x, frame_length, frame_step):
-    nch = x.shape[-1] // 2
-
-    x = [
-        tf.complex(real=x[:,:,:,2*i],imag=x[:,:,:,2*i+1]) for i in range(nch)
-    ]
+    in_x = tfk.Input(shape=input_shape)
     
-    x = [
-        tf.signal.inverse_stft(s, frame_length=frame_length, frame_step=frame_step) 
-        for s in x
-    ]
-    
-    x = tf.stack(x, axis=2)
+    x = in_x
+    for i, nf in enumerate(nfs):
+        x = tfkl.Conv1D(filters=nf, kernel_size=ks, padding='same', activation='relu')(x)
+        x = tfkl.Conv1D(filters=nf, strides=2, kernel_size=ks, padding='same', activation='relu')(x)
+        x = tfkl.Dropout(0.1)(x)
 
-    return x
+    x = tfkl.Flatten()(x)
+    x = tfkl.Dense(1, activation='sigmoid')(x)
+
+    return tfk.Model(inputs=in_x, outputs=x, name="discriminator")
+
+def dcgan(input_shape, unmixer, discriminator):
+    mixed_input = tfk.Input(shape=input_shape, name="mixed_input")    
+
+    unmixed_output = unmixer(mixed_input)
+        
+    disc_output = discriminator(unmixed_output)
+
+    return tfk.Model(inputs=mixed_input, outputs=[unmixed_output, disc_output], name="gan")
 
 def stft_unet(input_shape, nfs, ks, frame_length, frame_step):
     scale = 100.0
@@ -105,7 +104,7 @@ def stft_unet(input_shape, nfs, ks, frame_length, frame_step):
 
     in_x = tfk.Input(shape=input_shape)
 
-    x = stereo_stft(in_x, frame_length=frame_length, frame_step=frame_step) / scale
+    x = mixdata.stereo_stft(in_x, frame_length=frame_length, frame_step=frame_step) / scale
 
     layer_outputs = [x]
 
@@ -120,14 +119,19 @@ def stft_unet(input_shape, nfs, ks, frame_length, frame_step):
     for li in range(num_layers-2,-1,-1):
         x = upconv2d_block(x, layer_outputs[li], nfs[li], ks)
 
-    x = tfkl.Conv2D(4, kernel_size=(3,3), padding='same')(x)
+    stft_out = tfkl.Conv2D(4, kernel_size=(3,3), padding='same', name='stft')(x) * scale
     
-    x = stereo_inverse_stft(x * scale, frame_length=frame_length, frame_step=frame_step)
+    x = mixdata.stereo_inverse_stft(stft_out, frame_length=frame_length, frame_step=frame_step)
     
-    return tfk.Model(inputs=in_x, outputs=x)
+    return tfk.Model(inputs=in_x, outputs=[x, stft_out])
 
 
 if __name__ == "__main__":
-    model = stft_unet((131072,2), nfs= [ 32, 64, 128, 256 ])
-    #model = unet((1000000,2), nfs=[10,20,30,40,50])
-    print(model.summary())
+    from tensorflow.keras.utils import plot_model
+
+    shp = (131072,2)
+    unmixer = unet(shp, nfs=[10,20,30,40,50], ks=9)
+    plot_model(unmixer, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+    #disc = discriminator(shp, nfs= [ 30, 60, 90, 120, 150, 180, 210, 240 ], ks=9)
+    #gan = dcgan(shp, unmixer, disc)
+    #print(gan.summary())
--- train.py
+++ train.py
@@ -2,22 +2,22 @@ import os
 
 import tensorflow as tf
 import tensorflow.keras as tfk
-
-
+import numpy as np
 
 import model as mixmodel
 import dataset as mixdataset        
 
 def train():
-    N = 2048
+    N_tracks = 512
+    samples_per_track = 8
     epochs = 100
     batch_size = 4
     seq_dur = 3.0
     target = 'drums'
-    unet_filters = [ 30, 60, 90, 120, 150, 180, 210, 240 ]
+    unet_filters = [ 32, 64, 96, 128, 160, 192 ]
     checkpoint_dir = "./training"        
 
-    optimizer = tfk.optimizers.Adam(lr=0.0001)    
+    optimizer = tfk.optimizers.Adam(learning_rate=0.0001)    
 
     model = mixmodel.unet((131072,2), unet_filters, ks=9)
     model.compile(loss='mse', optimizer=optimizer)
@@ -34,8 +34,8 @@ def train():
 
     #ds = mixdataset.MusdbData('D:/MUSDB18/FullHQ', is_wav=True)
     ds = mixdataset.MusdbData('D:/MUSDB18/Full', is_wav=False, target=target)
-    train_ds = ds.random_dataset(N, seq_dur, subset='train')
-    val_ds = ds.random_dataset(batch_size*16, seq_dur, subset='val', augment=False)
+    train_ds = ds.random_dataset(N_tracks, samples_per_track, seq_dur, subset='train', augment=True)
+    val_ds = ds.random_dataset(batch_size*4, samples_per_track, seq_dur, subset='val', augment=False)
         
     class CustomCallback(tfk.callbacks.Callback):
         def on_epoch_end(self, epoch, logs=None):
@@ -56,12 +56,14 @@ def train_ft():
     seq_dur = 3.0
     target = 'drums'
     unet_filters =[ 32, 64, 128, 256, 512 ]
-    checkpoint_dir = "./training_ft"        
+    checkpoint_dir = "./training_ft"       
+    frame_length=512
+    frame_step=512 
 
     optimizer = tfk.optimizers.Adam(lr=0.0001)
 
-    model = mixmodel.stft_unet((131072,2), unet_filters, ks=(3,3), frame_length=512, frame_step=512)
-    model.compile(loss='mse', optimizer=optimizer)
+    model = mixmodel.stft_unet((131072,2), unet_filters, ks=(3,3), frame_length=frame_length, frame_step=frame_step)
+    model.compile(loss=[None,'mse'], optimizer=optimizer)
     print(model.summary())
 
     checkpoint = tf.train.Checkpoint(model=model, optimizer=optimizer)
@@ -75,8 +77,8 @@ def train_ft():
 
     #ds = mixdataset.MusdbData('D:/MUSDB18/FullHQ', is_wav=True)
     ds = mixdataset.MusdbData('D:/MUSDB18/Full', is_wav=False, target=target)
-    train_ds = ds.random_dataset(N_tracks, samples_per_track, seq_dur, subset='train')
-    val_ds = ds.random_dataset(batch_size*4, samples_per_track, seq_dur, subset='val', augment=False)
+    train_ds = ds.random_ft_dataset(N_tracks, samples_per_track, seq_dur, frame_length, frame_step, subset='train', augment=True)
+    val_ds = ds.random_ft_dataset(batch_size*4, samples_per_track, seq_dur, frame_length, frame_step, subset='val', augment=False)
         
     class CustomCallback(tfk.callbacks.Callback):
         def on_epoch_end(self, epoch, logs=None):
@@ -89,4 +91,80 @@ def train_ft():
         callbacks=[CustomCallback()]
     )
 
-if __name__ == "__main__": train_ft()
+def train_gan():
+    N_tracks = 512
+    samples_per_track = 8
+    epochs = 100
+    batch_size = 8
+    seq_dur = 3.0
+    target = 'drums'
+    unet_filters = [ 30, 60, 90, 120, 150, 180, 210, 240 ]
+    disc_filters = [ 30, 60, 90, 120, 150, 180, 210, 240 ]
+    checkpoint_dir = "./training_gan"       
+    shape = (131072,2)
+    
+    disc_optimizer = tfk.optimizers.Adam(learning_rate=0.001)#, clipnorm=1, beta_1=0.5)    
+    gan_optimizer = tfk.optimizers.Adam(learning_rate=0.001)#, clipnorm=1, beta_1=0.5)    
+    
+    discriminator = mixmodel.discriminator(shape, disc_filters, ks=9)
+    discriminator.compile(optimizer=disc_optimizer, loss='binary_crossentropy')
+
+    unmixer = mixmodel.unet(shape, unet_filters, ks=9)
+    discriminator.trainable = False    
+    gan = mixmodel.dcgan(shape, unmixer, discriminator)
+    #gan.compile(optimizer=gan_optimizer, loss=['mse','binary_crossentropy'])
+    gan.compile(optimizer=gan_optimizer, loss=['mse','binary_crossentropy'], loss_weights=[0.1, 0.9])
+
+    print(gan.summary())
+    print(discriminator.summary())
+    
+    checkpoint = tf.train.Checkpoint(
+        unmixer=unmixer,
+        discriminator=discriminator,
+        gan=gan,
+        disc_optimizer=disc_optimizer,
+        gan_optimizer=gan_optimizer
+    )
+        
+    manager = tf.train.CheckpointManager(checkpoint, directory=checkpoint_dir, max_to_keep=5)
+    checkpoint.restore(manager.latest_checkpoint)    
+
+    if manager.latest_checkpoint:
+        print(f"restored from {manager.latest_checkpoint}")
+    else:
+        print("training from scratch")
+
+    #ds = mixdataset.MusdbData('D:/MUSDB18/FullHQ', is_wav=True)
+    ds = mixdataset.MusdbData('D:/MUSDB18/Full', is_wav=False, target=target)
+    train_ds = ds.random_dataset(N_tracks, samples_per_track, seq_dur, subset='train', augment=True)
+    val_ds = ds.random_dataset(batch_size*4, samples_per_track, seq_dur, subset='val', augment=False)
+
+    for epoch in range(epochs):
+        for i, (mixed, unmixed) in enumerate(train_ds.batch(batch_size)):            
+            if i % 2 == 0:
+                disc_loss_r, disc_loss_f = train_discriminator_step(mixed, unmixed, discriminator, unmixer)
+                print(f"e:{epoch} b:{i} dlr:{disc_loss_r} dlf:{disc_loss_f}")
+            else:
+                gan_loss = train_gan_step(mixed, unmixed, gan)
+                print(f"e:{epoch} b:{i} gl:{gan_loss}")
+
+        manager.save()
+
+
+def train_discriminator_step(mixed, unmixed, discriminator, unmixer):    
+    y_real = np.ones(mixed.shape[0]) 
+    y_fake = np.zeros(mixed.shape[0])
+    
+    unmixed_pred = unmixer.predict(mixed)    
+    
+    loss_real = discriminator.train_on_batch(unmixed, y_real)
+    loss_fake = discriminator.train_on_batch(unmixed_pred, y_fake)
+
+    return loss_real, loss_fake
+
+def train_gan_step(mixed, unmixed, gan):    
+    y_gan = np.ones(mixed.shape[0])    
+    return gan.train_on_batch(mixed, [unmixed, y_gan])    
+
+
+if __name__ == "__main__": train()
